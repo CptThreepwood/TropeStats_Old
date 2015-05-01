93fb336198a7c174735398aef5773b240c132e92
David Jennens <d.jennens@gmail.com>
2015-05-02 04:27:59 +1000

    Some tidying up and adding a list of forbidden namespaces.  Basically this is any namespace that doesn't have a /Main/ page to go with it.  The idea being these are not super-tropes.  Get the scraper to do this in future (also verify that the current list is correct).

--- TvTropeScrape.py
+++ TvTropeScrape.py
@@ -1,43 +1,91 @@
 import urllib
 import re
-import json 
 from bs4 import BeautifulSoup
 
-trope_list = set()
+import json 
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
+tropes_visited = set()
 tropes = {}
-media_list = set()
+media_visited = set()
 media = {}
 allowedMedia = [
-    '/manga/',
-    '/comicbook/',
-    '/franchise/',
-    '/fanfic/',
-    '/webanimation/',
-    '/westernanimation/',
-    '/music/',
-    '/film/',
-    '/literature/',
-    '/series/',
-    '/videogame/',
-    '/webcomic/',
-    '/visualnovel/',
-    '/roleplay/',
-    '/webvideo/',
+    'comicbook',
+    'fanfic',
+    'film',
+    'franchise',
+    'letsplay',
+    'lightnovel',
+    'literature',
+    'manga',
+    'machinima',
+    'magazine',
+    'music',
+    'roleplay',
+    'series',
+    'TabletopGame',
+    'Toys',
+    'videogame',
+    'visualnovel',
+    'webanimation',
+    'webcomic',
+    'website',
+    'webvideo',
+    'westernanimation',
+    'wiki',
     ]
 
+# Can I work out a way to scrape this?  Maybe using the full list and hardcode some specific allowed types?
+# This will do for now
 ignoredTypes = [
     # Ignore languages other than English (for now)
-    '/no/',
-    '/pt/',
-    '/fi/',
-    '/it/',
-    '/de/',
-    '/fr/',
-    '/eo/',
-    '/es/',
-    '/ro/',
-    '/se/',
+    'de', 'eo', 'es', 'fi', 'fr', 'hu', 'it', 'itadministrivia', 'itdarthwiki', 'itsugarwiki', 'no', 'pl' 'pt', 'ro', 'se', 
+    
+    # Ignore YMMV type namespaces
+    'aatafovs', 'accidentalnightmarefuel', 'alternativecharacterinterpretation', 'andthefandomrejoiced', 'analysis', 'awesome', 
+    'awesomebosses', 'awesomebutimpractical', 'awesomemusic', 'badass', 'betterthanitsounds', 'fetishfuel', 'fridge', 'fridgebrilliance', 'fridgehorror', 
+    'funny', 'headscratchers', 'highoctanenightmarefuel', 'horrible', 'narm', 'nightmarefuel', 'shockingelimination', 'thatoneboss',
+    'thescrappy', 'whatanidiot', 'ymmv',
+
+    # Ignore TVtropes pages not relevant to project
+    'administrivia', 'charactersheets', 'characters', 'community', 'cowboybebopathiscomputer', 'creatorkiller', 'crimeandpunishmentseries', 
+    'darthwiki', 'dieforourship', 'drinkinggame', 'encounters', 'fanficrecs', 'fannickname', 'fishytheascendant', 'funwithacronyms', 'gush', 'haiku', 
+    'hellisthatnoise', 'hoyay', 'imagesource', 'justforfun', 'madmanentertainment', 'masseffect', 'memes', 'mysteryfiction', 'pantheon', 'pinball', 
+    'quotes', 'reallife', 'recap', 'referencedby', 'ride', 'sandbox', 'selfdemonstrating', 'slidingscale', 'soyouwantto', 'sugarwiki', 'thatoneboss',
+    'trivia', 'tropeco', 'tropers', 'tropertales', 'troubledproduction', 'turnofthemillennium', 'usefulnotes', 'warpthataesop', 'wmg', 'workpagesinmain',
+    'monster', 'wallbangers',
     ]
+
+# Namespaces allowed (basically super-tropes), they have a main page:
+# LyricalDissonance
+# NoodleIncident
+# RunningGag
+# Woobie
+# Wham Episode
+# TropeNamers - Special Parsing for this I think
+# TearJerker
+# TakeThat
+# ShoutOut
+# Radar
+# LampshadeHanging
+# FiveManBad
+# FiveManBand
+# FamousLastWords
+# EpicFail
+# EnsembleDarkHorse
+# Earworm
+# Creator
+# CrazyPrepared
+# ContinuityNod
+# BerserkButton
+# ArtEvolution
+# AnachronismStew 
+# ActorAllusion
 
 tvtropes_base = "tvtropes.org"
 tvtropes_main = "http://tvtropes.org"
@@ -174,29 +222,32 @@ def start_at_top():
 
 if __name__ == "__main__":
     # Let's set up some tests
+    outJSON = open("test.json", "w")
     
     # Trope parsing test
-    #URLs = parse_trope("http://tvtropes.org/pmwiki/pmwiki.php/Main/ChekhovsArmoury") 
-    #print "URLS TO VISIT"
-    #print URLs
-    #print
+    URLs = parse_trope("http://tvtropes.org/pmwiki/pmwiki.php/Main/ChekhovsArmoury") 
+
+    # Media parsing test
+    URLs += parse_media("http://tvtropes.org/pmwiki/pmwiki.php/Manga/Monster") 
 
     # Recursive search test
-    recur_search("http://tvtropes.org/pmwiki/pmwiki.php/Main/ChekhovsArmoury")
-    outJSON = open("test.json", "w")
+    #recur_search("http://tvtropes.org/pmwiki/pmwiki.php/Main/ChekhovsArmoury")
 
+    print "URLS TO VISIT"
+    for URL in URLs:
+        print URL
     print
     print "MEDIA INDEX:"
-    print json.dumps(media) 
-    json.dump(media, outJSON, indent = 4)
+    print json.dumps(media, cls=SetEncoder) 
+    json.dump(media, outJSON, indent = 4, cls=SetEncoder)
     print
     print "TROPE INDEX:"
-    print json.dumps(tropes)
-    json.dump(tropes, outJSON, indent = 4)
+    print json.dumps(tropes, cls=SetEncoder)
+    json.dump(tropes, outJSON, indent = 4, cls=SetEncoder)
     print
     print "MEDIA VISITED:"
-    print media_list
+    print media_visited
     print
     print "TROPES VISITED:"
-    print trope_list
+    print tropes_visited
     print
