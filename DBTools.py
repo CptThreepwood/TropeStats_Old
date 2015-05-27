88acc209fc132f11f0b24d331cfa99c40bc2d63b
David Jennens <d.jennens@gmail.com>
2015-05-27 20:38:56 +1000

    Move database creation into DBTools

--- DBTools.py
+++ DBTools.py
@@ -41,4 +41,36 @@ def load_tropes(dbconnection):
     all_tropes = dbcursor.fetchall()
     return all_tropes
 
+def initialiseDB():
+    # Connect to DB
+    if os.path.isfile('TropeStats.db'):
+        connection = sqlite3.connect('TropeStats.db')
+        return connection
+    # Create DB
+    else:
+        connection = sqlite3.connect('TropeStats.db')
+        cursor = connection.cursor()
+
+        # Build Tables
+        cursor.execute('''CREATE TABLE Media
+                        (MediaName text NOT NULL,
+                         MediaUrl text,
+                         MediaTitle text,
+                         CONSTRAINT PK_Name PRIMARY KEY(MediaName))''')
+        cursor.execute('''CREATE TABLE Tropes
+                       (TropeName text NOT NULL,
+                        TropeUrl text,
+                        TropeTitle text,
+                        CONSTRAINT PK_Trope PRIMARY KEY(TropeName))''')
+        cursor.execute('''CREATE TABLE MediaTropes
+                       (Media text NOT NULL, 
+                        Trope text NOT NULL, 
+                        Strength real,
+                        Direction int,
+                        CONSTRAINT PK_MediaTrope PRIMARY KEY(Media,Trope))''')
+#                 CONSTRAINT FK_MediaName FOREIGN KEY (Media) References Media(MediaName)
+#                 CONSTRAINT FK_TropeName FOREIGN KEY (Trope) References Tropes(TropeName))''')
+
+        connection.commit()
+        return connection
 
